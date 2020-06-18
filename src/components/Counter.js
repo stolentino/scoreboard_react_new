75ca3daffbba2441caac01474ba1f15d568f5064
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-06-17 21:58:42 -0700

    lift counter state up to App component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,29 @@
-//import React from 'react';
-import React, { Component } from 'react';
+import React from 'react';
+//import React, { Component } from 'react';
 
 //class Counter extends React.Component {
-class Counter extends Component {
-    state = { 
-      score: 0 
-    };
+//class Counter extends Component {
+ const Counter = (props) => {    
     
-    incrementScore = () => {
-      this.setState( prevState => ({
-        score: prevState.score + 1
-      }));
-    }
+    // incrementScore = () => {
+    //   this.setState( prevState => ({
+    //     score: prevState.score + 1
+    //   }));
+    // }
   
-    decrementScore = () => {
-      this.setState( prevState => ({
-        score: prevState.score - 1
-      }));
-    }
+    // decrementScore = () => {
+    //   this.setState( prevState => ({
+    //     score: prevState.score - 1
+    //   }));
+    // }
   
-    render() {
       return (
         <div className="counter">
-          <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-          <span className="counter-score">{ this.state.score }</span>
-          <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+          <button className="counter-action decrement"> - </button>
+          <span className="counter-score">{ props.score }</span>
+          <button className="counter-action increment"> + </button>
         </div>
       );
-    }
   }
 
   export default Counter;
