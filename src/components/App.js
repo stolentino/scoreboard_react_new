70ce072d3cad8edbc2fb25d2737f4502b065811c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-07-25 13:59:05 -0700

    update players base on previous state

--- src/components/App.js
+++ src/components/App.js
@@ -42,17 +42,20 @@ class App extends Component {
   }
 
   handleAddPlayer = (name) => {
-    this.setState({
-      players: [
-        ...this.state.players,
-        {
-          name: name,
-          score: 0,
-          id: this.prevPlayerId += 1,
-        }
-    ]
+    this.setState( prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name: name,
+            score: 0,
+            id: this.prevPlayerId += 1,
+          }
+        ]
+      };
     });
   }
+
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
       return {
