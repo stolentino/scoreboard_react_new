a8efcd9d877b3af5376f50e86e615770062c6189
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-10-26 14:44:58 -0700

    use refs to access input values

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 
 class AddPlayerForm extends Component {
 
-    state = {
+    /*state = {
         value: ''
-    };
+    };*/
 
-    handleValueChange = (e) => {
+    playerInput = React.createRef();
+
+    /*handleValueChange = (e) => {
         this.setState({value: e.target.value});
-    }
+    }*/
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
-        this.setState({value: ''});
+        //this.props.addPlayer(this.state.value);
+        this.props.addPlayer(this.playerInput.current.value);
+        //this.setState({value: ''});
+        e.currentTarget.reset();
     }
 
     render() {
-        console.log(this.state.value);
+        //console.log(this.state.value);
+        //console.log(this.playerInput);
         return (
             <form onSubmit={this.handleSubmit}>
                 <input
                     type="text"
-                    value={this.state.value}
-                    onChange={this.handleValueChange}
+                    ref={this.playerInput}
+                    //value={this.state.value}
+                    //onChange={this.handleValueChange}
                     placeholder="Enter a player's name"
                 />
 
