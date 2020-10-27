991fd9d7a8196ab80e455c5b4c4f617fcfd2d10a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-10-27 10:33:26 -0700

    add function to get highScore and pass to player

--- src/components/App.js
+++ src/components/App.js
@@ -64,7 +64,18 @@ class App extends Component {
     });
   }
 
+  getHighScore = () => {
+    const scores = this.state.players.map( p => p.score);
+    const highScore = Math.max(...scores);
+    if(highScore){
+      return highScore;
+    }
+    return null;
+  }
+
   render() {
+    const highScore = this.getHighScore();
+
     return (
       <div className="scoreboard">
         <Header 
@@ -81,7 +92,8 @@ class App extends Component {
             key={player.id.toString()} 
             index={index}
             changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}           
+            removePlayer={this.handleRemovePlayer}   
+            isHighScore={highScore === player.score}       
           />
         )}
         <AddPlayerForm addPlayer={this.handleAddPlayer}/>
