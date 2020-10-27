29026a25387d24e6cc993e6db54c63dbeb9227f3
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-10-26 18:43:38 -0700

    add static propTypes

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddPlayerForm extends Component {
 
     /*state = {
         value: ''
     };*/
+    static propTypes = {
+        addPlayer: PropTypes.func
+    };
 
     playerInput = React.createRef();
 
